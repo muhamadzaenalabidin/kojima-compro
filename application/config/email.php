389374b850ed2d6ee9af965a8e4457027508dbca
muhamadzaenalabidin <muhamadzaenalab@gmail.com>
2025-05-19 11:23:10 +0700

    add modul email

--- application/config/email.php
+++ application/config/email.php
@@ -1,9 +1,9 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-$config['protocol']  = 'smtp';
-$config['smtp_host'] = 'smtp.gmail.com';
-$config['smtp_port'] = 587;
+$config['protocol']  = '';
+$config['smtp_host'] = '';
+$config['smtp_port'] = 0;
 $config['smtp_user'] = '';
 $config['smtp_pass'] = '';
 $config['smtp_crypto'] = 'tls';
